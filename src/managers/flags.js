604914df5065672f2ff1eeca7e33c595788ac303
Unknown <blandblom@gmail.com>
2016-10-03 21:33:02 -0600

    Flags: Core foundation

--- src/managers/flags.js
+++ src/managers/flags.js
@@ -4,8 +4,13 @@
 function Flags(_flags) {
 	"use strict";
 
-	var _getFlagValuesFromHash,
+	var _init, _isValidFlag,
+		_convertToBoolean,
+		_getFlagValuesFromHash,
+		_internalFlags = {},
 		_api = this;
+
+	const REGEXP_HASHFLAG = /flags\[([A-Za-z0-9\-\_\&=]+)]/i;
 
 
 
@@ -13,7 +18,7 @@ function Flags(_flags) {
 	// If: The flags was called as a function and not as a constructor.
 	// Then: Throw an exception and prevent the API from being returned.
 	if (typeof this === "undefined" || typeof this.constructor === "undefined" || this.constructor.name !== "Flags") {
-		throw new SyntaxError(`Flags is not a function, use it as a constructor. Usage: var myFlags = new Flags({})`);
+		throw new SyntaxError(`Flags is not a function, use it as a constructor. Usage: var flags = new Flags({})`);
 	}
 
 
@@ -31,13 +36,95 @@ function Flags(_flags) {
 	}
 
 
-	/************************* Helper Methods *************************/
-	_getFlagValuesFromHash = function () {
 
+	/************************* Helper Methods *************************/
+	_init = function () {
+		var key, value, hashFlags;
+
+		hashFlags = _getFlagValuesFromHash();
+
+		for (key in _flags) {
+			value = (typeof hashFlags[key] === "boolean")
+				? hashFlags[key]
+				: _flags[key];
+
+			if (_isValidFlag(key, value)) {
+				Object.defineProperty(_api, key, {
+					get: () => {
+						return _internalFlags[key];
+					},
+					set: val => _internalFlags[key] = _convertToBoolean(val)
+				});
+
+				_internalFlags[key] = _convertToBoolean(value);
+			}
+		}
 	};
+
+
+	_isValidFlag = function (key, value) {
+		return (
+			typeof key === "string"
+			&& typeof value === "boolean"
+			&& key.trim() !== ""
+		);
+	};
+
+
+	_convertToBoolean = function (value) {
+		var bool;
+
+		if (typeof value === "boolean") {
+			bool = value;
+		}
+		else if (typeof value === "number") {
+			bool = (value === 1)
+				? true
+				: false;
+		}
+		else {
+			throw new SyntaxError();
+		}
+
+		return bool;
+	};
+
+
+	_getFlagValuesFromHash = function () {
+		var hashFlags = {},
+			regexpResult;
+
+		regexpResult = REGEXP_HASHFLAG.exec(location.hash);
+
+		if (Array.isArray(regexpResult) && regexpResult.length === 2) {
+			regexpResult[1]
+				.split("&")
+				.forEach(flagStr => {
+					var key, value, parts;
+
+					parts = flagStr.split("=");
+					key = parts[0];
+					value = (typeof parts[1] === "string")
+						? parts[1]
+						: true;
+
+					if (_isValidFlag(key, value)) {
+						hashFlags[key] = value;
+					}
+				});
+		}
+
+		return hashFlags;
+	};
+
+
+
+	/************************* Initialize *************************/
+	_init();
+
 
 
 	/************************* Return API *************************/
 	// Object.seal() allows for the values of the flags to be changed, but not the list itself.
-	return Object.seal(_flags);
+	return Object.seal(_api);
 };
--- tests/managers/flags.js
+++ tests/managers/flags.js
@@ -51,4 +51,81 @@ describe("Flags", function() {
 		expect(() => Flags.bind()())
 			.to.throwError();
 	});
+
+
+
+	/*************** Tests ***************/
+	it("Initial Flag Values", function() {
+		expect(flags["developer-mode"])
+			.to.be(true);
+
+		expect(flags["new-feature"])
+			.to.be(false);
+	});
+
+
+	it("Set Flag Values", function() {
+		flags["developer-mode"] = false;
+		flags["new-feature"] = true;
+
+		expect(flags["developer-mode"])
+			.to.be(false);
+
+		expect(flags["new-feature"])
+			.to.be(true);
+	});
+
+
+	it("0 or 1 Flag Values (resolves to true or false)", function() {
+		flags["developer-mode"] = 0;
+		flags["new-feature"] = 1;
+
+		expect(flags["developer-mode"])
+			.to.be(false);
+
+		expect(flags["new-feature"])
+			.to.be(true);
+	});
+
+
+	// it("Callback Response: Testing typeof", function() {
+	// 	expect(flags["new-feature"])
+	// 		.to.be(false);
+	// }
+
+
+
+	// 	messenger1
+	// 		.listen("subTest", function(arg0, arg1, arg2, arg3, arg4, arg5, arg6) {
+	// 			try {
+	// 				expect(typeof arg0)
+	// 					.to.be("object")
+
+	// 				expect(typeof arg1)
+	// 					.to.be("object")
+
+	// 				expect(typeof arg2)
+	// 					.to.be("number")
+
+	// 				expect(typeof arg3)
+	// 					.to.be("string")
+
+	// 				expect(typeof arg4)
+	// 					.to.be("boolean")
+
+	// 				expect(typeof arg5)
+	// 					.to.be("function")
+
+	// 				expect(typeof arg6)
+	// 					.to.be("undefined")
+
+	// 				done();
+	// 			}
+	// 			catch (ex) {
+	// 				done(ex);
+	// 			}
+	// 		});
+
+	// 	messenger1.post("subTest", {}, [], 99, "my string", false, function() {});
+	// });
 });
