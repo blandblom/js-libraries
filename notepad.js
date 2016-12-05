281ff4fd7f3d4b38dba8db671209d2d2675e0ec3
Brian Landblom <blandblom@flagshipbio.com>
2016-12-04 18:53:46 -0700

    Base Framework API Ideas

--- notepad.js
+++ notepad.js
@@ -182,29 +182,34 @@ var myapp = new function() {
 
 
 
-	/************************* Configuration *************************/
-	_bootloaderConfig = {
-		requireMappings: {
-			// Configuration
-			config: "/js/_config.js",
+	/************************* Require Mappings *************************/
+	_requireMappings = {
+		// Configuration
+		config: "/js/_config.js",
 
-			// Application
-			dom: "/js/_app.js#dom",
-			enums: "/js/_app.js#enums",
-			flags: "/js/_app.js#flags",
-			helpers: "/js/_app.js#helpers",
-			logger: "/js/_app.js#logger",
-			messenger: "/js/_app.js#messenger",
-			router: "/js/_app.js#router",
-			svc: "/js/_app.js#svc",
-			user: "/js/_app.js#user",
-			util: "/js/_app.js#util",
+		// SAMPLE - Assumed to be the first on the page (should only be 1 definition in this case but that is not enforced)
+		nameOfDefinition: "/js/sample.js",
 
-			// Objects
-			Component: "",
-			Logger: "",
-			Messenger: ""
-		}
+		// SAMPLE - Not assumed, must give the name of the definition
+		nameOfDefinition: "/js/sample.js#name-of-definition",
+
+		// Application
+		enums: "/js/_app.js#enums",
+		flags: "/js/_app.js#flags",
+		helpers: "/js/_app.js#helpers",
+		svc: "/js/_app.js#svc",
+		user: "/js/_app.js#user",
+		util: "/js/_app.js#util",
+
+		// Framework
+		component: "/js/_framework.js#component",
+		dom: "/js/_framework.js#dom",
+		logger: "/js/_framework.js#logger",
+		messenger: "/js/_framework.js#messenger",
+		router: "/js/_framework.js#router",
+
+		// Component: My Component
+		myComponent_ModelPartA: "/components/my-component.js#modelPartA"
 	};
 
 
@@ -234,11 +239,8 @@ var myapp = new function() {
 	};
 
 
-	_createDependency = function	(options) {
-		var dependency,
-			config, util, flags, enums, dom,
-			messenger, logger, router,
-			svc, helpers, component;
+	_createDependency = function(options) {
+		var dependency;
 
 		//
 		dependency = {
@@ -274,7 +276,7 @@ var myapp = new function() {
 	// };
 
 
-	_api.define = function (options) {
+	_api.define = function(options) {
 		// Verify arguments
 		if (typeof options !== "object" || options === null) {
 			throw new SyntaxError(`The definition must be defined with the following object: { name: string, main: function, require: optional array<string> }.`);
@@ -305,7 +307,7 @@ var myapp = new function() {
 
 
 
-	_api.defineModel = function (options) {
+	_api.defineModel = function(options) {
 		// Verify arguments
 		if (typeof options !== "object" || options === null) {
 			throw new SyntaxError(`The model must be defined with the following object: { name: string, main: function, keys: optional array<string>, require: optional array<string> }.`);
@@ -340,7 +342,7 @@ var myapp = new function() {
 
 
 
-	_api.defineAction = function (options) {};
+	_api.defineAction = function(options) {};
 
 
 
